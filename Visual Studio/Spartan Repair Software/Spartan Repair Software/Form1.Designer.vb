2afddf17aee3925d7e0c100db09f6f32eb39e8b7
Royce Whitaker <roycewhitaker@rocketmail.com>
2017-10-31 13:25:52 -0700

    Started Visual Basic Project. Electron app working fine.

---
+++ Visual Studio/Spartan Repair Software/Spartan Repair Software/Form1.Designer.vb
@@ -0,0 +1,205 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.txtbox = New System.Windows.Forms.TextBox()
+        Me.btnOK = New System.Windows.Forms.Button()
+        Me.btnmessage = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.txtAddVal1 = New System.Windows.Forms.TextBox()
+        Me.btnAddValues = New System.Windows.Forms.Button()
+        Me.txtAddValue2 = New System.Windows.Forms.TextBox()
+        Me.lblAddEquel = New System.Windows.Forms.Label()
+        Me.txtSumAnswer = New System.Windows.Forms.TextBox()
+        Me.txtDivideAnswer = New System.Windows.Forms.TextBox()
+        Me.lblDivideEquel = New System.Windows.Forms.Label()
+        Me.txtDivide2 = New System.Windows.Forms.TextBox()
+        Me.btnDivideValues = New System.Windows.Forms.Button()
+        Me.txtDivide1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.Location = New System.Drawing.Point(13, 13)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(35, 13)
+        Me.lblName.TabIndex = 0
+        Me.lblName.Text = "Name"
+        '
+        'txtbox
+        '
+        Me.txtbox.Location = New System.Drawing.Point(54, 10)
+        Me.txtbox.Name = "txtbox"
+        Me.txtbox.Size = New System.Drawing.Size(100, 20)
+        Me.txtbox.TabIndex = 1
+        '
+        'btnOK
+        '
+        Me.btnOK.Location = New System.Drawing.Point(16, 46)
+        Me.btnOK.Name = "btnOK"
+        Me.btnOK.Size = New System.Drawing.Size(75, 23)
+        Me.btnOK.TabIndex = 2
+        Me.btnOK.Text = "&Ok"
+        Me.btnOK.UseVisualStyleBackColor = True
+        '
+        'btnmessage
+        '
+        Me.btnmessage.Location = New System.Drawing.Point(106, 46)
+        Me.btnmessage.Name = "btnmessage"
+        Me.btnmessage.Size = New System.Drawing.Size(75, 23)
+        Me.btnmessage.TabIndex = 3
+        Me.btnmessage.Text = "&Message"
+        Me.btnmessage.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(197, 46)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 4
+        Me.btnExit.Text = "&Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'txtAddVal1
+        '
+        Me.txtAddVal1.Location = New System.Drawing.Point(16, 99)
+        Me.txtAddVal1.Name = "txtAddVal1"
+        Me.txtAddVal1.Size = New System.Drawing.Size(55, 20)
+        Me.txtAddVal1.TabIndex = 5
+        '
+        'btnAddValues
+        '
+        Me.btnAddValues.Location = New System.Drawing.Point(77, 96)
+        Me.btnAddValues.Name = "btnAddValues"
+        Me.btnAddValues.Size = New System.Drawing.Size(23, 23)
+        Me.btnAddValues.TabIndex = 6
+        Me.btnAddValues.Text = "+"
+        Me.btnAddValues.UseVisualStyleBackColor = True
+        '
+        'txtAddValue2
+        '
+        Me.txtAddValue2.Location = New System.Drawing.Point(107, 98)
+        Me.txtAddValue2.Name = "txtAddValue2"
+        Me.txtAddValue2.Size = New System.Drawing.Size(55, 20)
+        Me.txtAddValue2.TabIndex = 7
+        '
+        'lblAddEquel
+        '
+        Me.lblAddEquel.AutoSize = True
+        Me.lblAddEquel.Location = New System.Drawing.Point(169, 102)
+        Me.lblAddEquel.Name = "lblAddEquel"
+        Me.lblAddEquel.Size = New System.Drawing.Size(13, 13)
+        Me.lblAddEquel.TabIndex = 8
+        Me.lblAddEquel.Text = "="
+        '
+        'txtSumAnswer
+        '
+        Me.txtSumAnswer.Location = New System.Drawing.Point(193, 98)
+        Me.txtSumAnswer.Name = "txtSumAnswer"
+        Me.txtSumAnswer.Size = New System.Drawing.Size(55, 20)
+        Me.txtSumAnswer.TabIndex = 9
+        '
+        'txtDivideAnswer
+        '
+        Me.txtDivideAnswer.Location = New System.Drawing.Point(193, 134)
+        Me.txtDivideAnswer.Name = "txtDivideAnswer"
+        Me.txtDivideAnswer.Size = New System.Drawing.Size(55, 20)
+        Me.txtDivideAnswer.TabIndex = 14
+        '
+        'lblDivideEquel
+        '
+        Me.lblDivideEquel.AutoSize = True
+        Me.lblDivideEquel.Location = New System.Drawing.Point(169, 138)
+        Me.lblDivideEquel.Name = "lblDivideEquel"
+        Me.lblDivideEquel.Size = New System.Drawing.Size(13, 13)
+        Me.lblDivideEquel.TabIndex = 13
+        Me.lblDivideEquel.Text = "="
+        '
+        'txtDivide2
+        '
+        Me.txtDivide2.Location = New System.Drawing.Point(107, 134)
+        Me.txtDivide2.Name = "txtDivide2"
+        Me.txtDivide2.Size = New System.Drawing.Size(55, 20)
+        Me.txtDivide2.TabIndex = 12
+        '
+        'btnDivideValues
+        '
+        Me.btnDivideValues.Location = New System.Drawing.Point(77, 132)
+        Me.btnDivideValues.Name = "btnDivideValues"
+        Me.btnDivideValues.Size = New System.Drawing.Size(23, 23)
+        Me.btnDivideValues.TabIndex = 11
+        Me.btnDivideValues.Text = "/"
+        Me.btnDivideValues.UseVisualStyleBackColor = True
+        '
+        'txtDivide1
+        '
+        Me.txtDivide1.Location = New System.Drawing.Point(16, 135)
+        Me.txtDivide1.Name = "txtDivide1"
+        Me.txtDivide1.Size = New System.Drawing.Size(55, 20)
+        Me.txtDivide1.TabIndex = 10
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.txtDivideAnswer)
+        Me.Controls.Add(Me.lblDivideEquel)
+        Me.Controls.Add(Me.txtDivide2)
+        Me.Controls.Add(Me.btnDivideValues)
+        Me.Controls.Add(Me.txtDivide1)
+        Me.Controls.Add(Me.txtSumAnswer)
+        Me.Controls.Add(Me.lblAddEquel)
+        Me.Controls.Add(Me.txtAddValue2)
+        Me.Controls.Add(Me.btnAddValues)
+        Me.Controls.Add(Me.txtAddVal1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnmessage)
+        Me.Controls.Add(Me.btnOK)
+        Me.Controls.Add(Me.txtbox)
+        Me.Controls.Add(Me.lblName)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblName As Label
+    Friend WithEvents txtbox As TextBox
+    Friend WithEvents btnOK As Button
+    Friend WithEvents btnmessage As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents txtAddVal1 As TextBox
+    Friend WithEvents btnAddValues As Button
+    Friend WithEvents txtAddValue2 As TextBox
+    Friend WithEvents lblAddEquel As Label
+    Friend WithEvents txtSumAnswer As TextBox
+    Friend WithEvents txtDivideAnswer As TextBox
+    Friend WithEvents lblDivideEquel As Label
+    Friend WithEvents txtDivide2 As TextBox
+    Friend WithEvents btnDivideValues As Button
+    Friend WithEvents txtDivide1 As TextBox
+End Class
---
+++ Visual Studio/Spartan Repair Software/Spartan Repair Software/Form1.vb
@@ -0,0 +1,57 @@
+﻿Public Class Form1
+    Private Sub lblName_Click(sender As Object, e As EventArgs) Handles lblName.Click
+
+    End Sub
+
+    Private Sub btnOK_Click(sender As Object, e As EventArgs) Handles btnOK.Click
+
+        txtbox.Text = "Hello " & txtbox.Text
+
+    End Sub
+
+    Private Sub btnmessage_Click(sender As Object, e As EventArgs) Handles btnmessage.Click
+        MessageBox.Show("Hello " & txtbox.Text,
+                        "Hello " & txtbox.Text)
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnAddValues_Click(sender As Object, e As EventArgs) Handles btnAddValues.Click
+
+        Try
+
+            Dim firstNum As Integer = CInt(txtAddVal1.Text)
+            Dim secondNum As Integer = CInt(txtAddValue2.Text)
+
+            Dim sum = firstNum + secondNum
+
+            txtSumAnswer.Text = CStr(sum)
+
+        Catch ex As System.InvalidCastException
+
+            MessageBox.Show("Please enter numbers to sum",
+                        "error")
+
+        Catch ex As Exception
+
+            MessageBox.Show("An unknown error has occured",
+                            "Error")
+
+        End Try
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles txtDivideAnswer.TextChanged
+
+    End Sub
+
+    Private Sub btnDivideValues_Click(sender As Object, e As EventArgs) Handles btnDivideValues.Click
+        Dim firstNum As Decimal = CDec(txtDivide1.Text)
+        Dim secondNum As Decimal = CDec(txtDivide2.Text)
+        Dim intSolution As Integer = CType(firstNum / secondNum, Integer)
+
+        txtDivideAnswer.Text = CStr(intSolution)
+    End Sub
+End Class
